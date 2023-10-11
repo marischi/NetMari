fecaf261d339d4dba9959d7972d7f0596b1fcb0c
Marilene Esquiavoni <cg091571@LAB202PROF.IFSP>
2023-10-11 10:51:06 -0300

    Finalizando o menu mobile

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*VARIÁVEIS NAVBAR*/
+const navOpenBtn = document.querySelector("[data-menu-open]");
+const navCloseBtn = document.querySelector("[data-menu-close]");
+const navBar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const nav = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < nav.length; i++) {
+  nav[i].addEventListener("click", function () {
+    navBar.classList.toggle("active");
+  });
+}
